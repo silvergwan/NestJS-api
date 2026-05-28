7048ac6868c9bea242adb5018f2466cf59f8e0c0
EUNGWANCHOI <silvergwan@gmail.com>
2026-05-28 21:27:22 +0900

    feat: mongodb와 연결?

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StaysModule } from './stays/stays.module';
 
 @Module({
-  imports: [StaysModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/fineStay'), StaysModule],
   controllers: [AppController],
   providers: [AppService],
 })
