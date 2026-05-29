d5d14cf3beeb8c020a29c218215b8c4ab1d22bcd
EUNGWANCHOI <silvergwan@gmail.com>
2026-05-29 10:49:41 +0900

    update: reservation 모듈 임포트

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StaysModule } from './stays/stays.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/fineStay'), StaysModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/fineStay'), StaysModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
