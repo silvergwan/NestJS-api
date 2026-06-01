613f5361faefa43d9c0ba693cf742654ffb88d8c
EUNGWANCHOI <silvergwan@gmail.com>
2026-06-01 19:19:07 +0900

    feat: main.ts에 검증 파이프 등록(DTO)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 전역 검증 파이프 등록
+  // 요청이 들어올 때마다 DTO 규칙대로 자동 검증
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,            // DTO에 없는 필드는 자동으로 제거
+      forbidNonWhitelisted: true, // DTO에 없는 필드 들어오면 400 에러
+      transform: true,            // 타입 자동 변환 (string → number 등)
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
